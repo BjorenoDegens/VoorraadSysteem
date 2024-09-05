b1fff89d7cc899a95faa6d229363fc5eede881ff
Naser Jafari <naserjafari5002@gmail.com>
2024-09-05 10:54:47 +0200

    bootstrap toegevoegd 5,3, begin producten en categorie tabel

---
+++ VoorraadSysteem/resources/views/product/index.blade.php
@@ -0,0 +1,12 @@
+<x-layout title="Producten index">
+    <div class="container">
+        <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Email address</label>
+            <input id="exampleFormControlInput1" type="email" class="form-control" placeholder="name@example.com">
+        </div>
+        <div class="mb-3">
+            <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+            <textarea id="exampleFormControlTextarea1" class="form-control" rows="3"></textarea>
+        </div>
+    </div>
+</x-layout>
