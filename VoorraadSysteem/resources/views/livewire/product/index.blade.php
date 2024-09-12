1ea6e481f0d6d4f5960c963d3837899c4361f7d9
Naser Jafari <naserjafari5002@gmail.com>
2024-09-12 11:06:06 +0200

    product tables en component for alert msg

--- VoorraadSysteem/resources/views/livewire/product/index.blade.php
+++ VoorraadSysteem/resources/views/livewire/product/index.blade.php
@@ -1,4 +1,12 @@
 <x-layout title="Producten index">
-    <h1>Producten</h1>
+    <div class="d-flex justify-content-between align-items-center mb-3">
+        <div>
+            <h1>Producten</h1>
+        </div>
+        <div>
+            <a href="{{ route('product.create') }}" class="btn btn-primary">Product toevoegen</a>
+        </div>
+    </div>
+    <x-alertmsg />
     <livewire:product-table />
 </x-layout>
