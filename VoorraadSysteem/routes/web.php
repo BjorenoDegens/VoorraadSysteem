69864a60e8ad18caa16d3874a67d3598e0640aa0
Suhaayb <1197145@student.roc-nijmegen.nl>
2025-01-07 08:55:15 +0100

    Bare admin page aangemaakt

--- VoorraadSysteem/routes/web.php
+++ VoorraadSysteem/routes/web.php
@@ -34,5 +34,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.dashboard');
+});
 require __DIR__ . '/auth.php';
