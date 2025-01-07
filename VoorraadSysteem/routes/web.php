b6837a71a0eca5878a56839365d9879c2586043f
Suhaayb <1197145@student.roc-nijmegen.nl>
2025-01-07 09:40:23 +0100

    Admin page werkt weer

--- VoorraadSysteem/routes/web.php
+++ VoorraadSysteem/routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ArchiveController;
+use App\Http\Controllers\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -37,4 +38,5 @@ Route::middleware('auth')->group(function () {
 Route::middleware(['auth', 'admin'])->group(function () {
     Route::get('/admin', [AdminController::class, 'index'])->name('admin.dashboard');
 });
+
 require __DIR__ . '/auth.php';
